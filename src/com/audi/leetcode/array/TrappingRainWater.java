006736c156b387d7fb8c4fa40fe896fe99c9c92d
wangquanzhou <audi.car@qq.com>
2020-04-15 20:12:08 +0800

    add trapping water

---
+++ src/com/audi/leetcode/array/TrappingRainWater.java
@@ -0,0 +1,23 @@
+package com.audi.leetcode.array;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water/
+ * <p>
+ * 基本思路，就是：某个位置能接多少雨水，首先确定她的左右挡板的最大值，再从两个最大值中选一个较小的，这个较小的值减去当前节点本身的高度，纪委当前节点可接水量
+ * <p>
+ * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ *
+ * @author WangQuanzhou
+ * @date 2020-04-15
+ */
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
