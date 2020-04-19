e4c9f99a9f9984b94bfa652883a5dc49627c7e4e
audi <audi.car@qq.com>
2020-04-19 14:07:12 +0800

    Longest Valid Parentheses

---
+++ src/com/audi/leetcode/str/LongestValidParentheses.java
@@ -0,0 +1,11 @@
+package com.audi.leetcode.str;
+
+
+/**
+ * https://leetcode.com/problems/longest-valid-parentheses/
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/4/19 14:04
+ */
+public class LongestValidParentheses {
+}
