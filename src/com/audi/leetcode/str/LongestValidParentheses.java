af60fb486de066a432e217a625774776cd856e38
wangquanzhou <audi.car@qq.com>
2020-04-21 19:09:38 +0800

    add comment

--- src/com/audi/leetcode/str/LongestValidParentheses.java
+++ src/com/audi/leetcode/str/LongestValidParentheses.java
@@ -3,6 +3,8 @@ package com.audi.leetcode.str;
 
 /**
  * https://leetcode.com/problems/longest-valid-parentheses/
+ * <p>
+ * 参考 https://zhuanlan.zhihu.com/p/41951874
  *
  * @author: WangQuanzhou
  * @date: 2020/4/19 14:04
