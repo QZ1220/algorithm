0216962e9f6e53f5655f26b65037f108e8ddeadb
wangquanzhou <audi.car@qq.com>
2020-04-23 20:28:39 +0800

    max sub array

---
+++ src/com/audi/leetcode/array/MaximumSubarray.java
@@ -0,0 +1,24 @@
+package com.audi.leetcode.array;
+
+/**
+ * https://leetcode.com/problems/maximum-subarray/
+ * <p>
+ * 使用dp求解
+ * <p>
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * @author WangQuanzhou
+ * @date 2020-04-23
+ */
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
