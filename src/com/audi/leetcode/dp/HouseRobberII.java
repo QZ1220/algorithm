295709103a967e6ffe226b4c3d1acf7f5ab9e2f9
audi <audi.car@qq.com>
2020-05-10 22:46:28 +0800

    add house robber

---
+++ src/com/audi/leetcode/dp/HouseRobberII.java
@@ -0,0 +1,19 @@
+package com.audi.leetcode.dp;
+
+/**
+ * https://leetcode.com/problems/house-robber-ii/
+ * <p>
+ * 这个题其实很类似于https://leetcode.com/problems/house-robber/
+ * <p>
+ * 唯一的区别是这里的商店是环状的
+ * <p>
+ * 这题的解法和https://leetcode.com/problems/house-robber/也很类似，只是需要考虑首尾的情况
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/5/10 22:42
+ */
+public class HouseRobberII {
+    public int rob(int[] nums) {
+        return 0;
+    }
+}
