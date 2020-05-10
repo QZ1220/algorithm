cdbf5763f5f1698490cc3190e44c1d6f32eed63b
audi <audi.car@qq.com>
2020-05-10 22:30:10 +0800

    add solution for unique path ii

--- src/com/audi/leetcode/dp/UniquePathsII.java
+++ src/com/audi/leetcode/dp/UniquePathsII.java
@@ -31,16 +31,27 @@ public class UniquePathsII {
         // 如果只有一列，那每个节点只能上一个节点直走，因此都是1
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+                break;
+            }
         }
         // 如果只有一行，那每个节点只能上一个节点直走，因此都是1
         for (int i = 0; i < n; i++) {
             dp[0][i] = 1;
+            if (obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0;
+                break;
+            }
         }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 // 递推公式：每个节点只能从左上或者前面节点走过来
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                }
             }
         }
         return dp[m - 1][n - 1];
