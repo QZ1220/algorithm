9acd9188dbe2003a0d5352b5c04d1a4c18e27c25
audi <audi.car@qq.com>
2020-05-16 22:21:39 +0800

    add longest incresing subsequeuence

---
+++ src/com/audi/leetcode/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,11 @@
+package com.audi.leetcode.dp;
+
+
+/**
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/5/16 22:20
+ */
+public class LongestIncreasingSubsequence {
+}
