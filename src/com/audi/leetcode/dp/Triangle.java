45f81632753b7ab37e0841379527de17cdcdfdda
audi <audi.car@qq.com>
2020-05-16 22:06:31 +0800

    add trangle

---
+++ src/com/audi/leetcode/dp/Triangle.java
@@ -0,0 +1,20 @@
+package com.audi.leetcode.dp;
+
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/triangle/
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/5/16 21:58
+ */
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
