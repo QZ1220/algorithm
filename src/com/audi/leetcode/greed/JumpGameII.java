7af9430414db7a965a9df831bb8196a78fd10153
audi <audi.car@qq.com>
2020-05-31 19:16:29 +0800

    add impl for JumpGameII

--- src/com/audi/leetcode/greed/JumpGameII.java
+++ src/com/audi/leetcode/greed/JumpGameII.java
@@ -8,4 +8,11 @@ package com.audi.leetcode.greed;
  * @date: 2020/5/31 19:14
  */
 public class JumpGameII {
+    public int jump(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
