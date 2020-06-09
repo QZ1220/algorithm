88fe993506e8762c385f567f91ac37fd56322497
wangquanzhou <audi.car@qq.com>
2020-06-09 20:58:17 +0800

    add Minimum Number of Arrows to Burst Balloons

---
+++ src/com/audi/leetcode/greed/MinimumNumberofArrowstoBurstBalloons.java
@@ -0,0 +1,31 @@
+package com.audi.leetcode.greed;
+
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+ * <p>
+ * 射爆所有的气球需要的最少射击次数
+ * <p>
+ * Input:
+ * [[10,16], [2,8], [1,6], [7,12]]
+ * <p>
+ * Output:
+ * 2
+ * <p>
+ * Explanation:
+ * One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+ *
+ * @author WangQuanzhou
+ * @date 2020-06-09
+ */
+public class MinimumNumberofArrowstoBurstBalloons {
+
+    public int findMinArrowShots(int[][] points) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
