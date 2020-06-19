fc529bb028e3fe7080729613d2cbe0399cb2fab4
wangquanzhou <audi.car@qq.com>
2020-06-19 20:42:10 +0800

    add subset

--- src/com/audi/leetcode/recursion/Subsets.java
+++ src/com/audi/leetcode/recursion/Subsets.java
@@ -1,6 +1,8 @@
 package com.audi.leetcode.recursion;
 
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,10 +14,35 @@ import java.util.List;
 public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+
+        List<Integer> item = new LinkedList<>();
+        // 放入空子集
+        result.add(item);
+        // 递归回溯
+        subset(0, nums, item, result);
+        return result;
+    }
+
+    private void subset(int i, int[] nums, List<Integer> item, List<List<Integer>> result) {
+        if (i >= nums.length) {
+            return;
+        }
+        item.add(nums[i]);
+        // 注意这里要新建一个对象放入result，不能直接放入item到result
+        result.add(new LinkedList<>(item));
+        // 选择nums[i+1]
+        subset(i + 1, nums, item, result);
+
+        // 不选择nums[i+1]
+        item.remove(item.size() - 1);
+        subset(i + 1, nums, item, result);
 
     }
 
     public static void main(String[] args) {
-
+        Subsets subsets = new Subsets();
+        int[] nums = {1, 2, 3};
+        System.out.println(subsets.subsets(nums));
     }
 }
