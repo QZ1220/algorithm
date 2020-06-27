c75c3cc01b99aff9024fe90aab092fdaafd67b24
audi <audi.car@qq.com>
2020-06-27 22:25:10 +0800

    add generate parenthess

---
+++ src/com/audi/leetcode/recursion/GenerateParentheses.java
@@ -0,0 +1,15 @@
+package com.audi.leetcode.recursion;
+
+
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/6/27 22:24
+ */
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+
+    }
+}
