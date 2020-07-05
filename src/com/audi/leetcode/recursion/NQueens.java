e7445ac72771f0fcfda1b2e7ab9239ba247503bf
audi <audi.car@qq.com>
2020-07-05 11:55:48 +0800

    add n queues

---
+++ src/com/audi/leetcode/recursion/NQueens.java
@@ -0,0 +1,23 @@
+package com.audi.leetcode.recursion;
+
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/n-queens/
+ * <p>
+ * N皇后问题
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/7/5 11:54
+ */
+public class NQueens {
+
+    public List<List<String>> solveNQueens(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
