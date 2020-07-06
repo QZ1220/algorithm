30b2ba8361c900770a449c4a18c79e572009f2f3
wangquanzhou <audi.car@qq.com>
2020-07-06 19:24:59 +0800

    add solution for GenerateParentheses

--- src/com/audi/leetcode/recursion/GenerateParentheses.java
+++ src/com/audi/leetcode/recursion/GenerateParentheses.java
@@ -1,6 +1,9 @@
 package com.audi.leetcode.recursion;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/generate-parentheses/
  *
@@ -9,7 +12,31 @@ package com.audi.leetcode.recursion;
  */
 public class GenerateParentheses {
 
-    public static void main(String[] args) {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> ret = new ArrayList<String>();
+        if (n <= 0)
+            return ret;
+        dfs(n, n, "", ret);
+        return ret;
+    }
 
+    private void dfs(int left, int right, String s, ArrayList<String> ret) {
+        if (left > right)
+            return;
+        else if (left == 0 && right == 0) {
+            ret.add(s);
+            return;
+        }
+        if (left > 0) {
+            dfs(left - 1, right, s + "(", ret);
+        }
+        if (right > 0) {
+            dfs(left, right - 1, s + ")", ret);
+        }
+    }
+
+    public static void main(String[] args) {
+        GenerateParentheses generateParentheses = new GenerateParentheses();
+        System.out.println(generateParentheses.generateParenthesis(3));
     }
 }
