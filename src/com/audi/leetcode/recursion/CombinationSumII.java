23a55350c5b6b27dbd8e4ca509b51efd5b76799e
audi <audi.car@qq.com>
2020-07-06 21:58:41 +0800

    add comment for CombinationSumII

--- src/com/audi/leetcode/recursion/CombinationSumII.java
+++ src/com/audi/leetcode/recursion/CombinationSumII.java
@@ -1,15 +1,23 @@
 package com.audi.leetcode.recursion;
 
 
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/combination-sum-ii/
+ * <p>
+ * 给出一组数字nums，以及一个目标值target，问能找到多少个nums的子集，使得子集的和为target
+ * <p>
+ * 其实这道题和SubsetsII类似，只是初始时以及循环的过程需要做一些优化
  *
  * @author: WangQuanzhou
  * @date: 2020/6/27 22:14
  */
 public class CombinationSumII {
 
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
+    }
 
     public static void main(String[] args) {
 
