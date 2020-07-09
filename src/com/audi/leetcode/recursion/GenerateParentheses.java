867d36a939a5735945c97838da1439d43a7df477
wangquanzhou <audi.car@qq.com>
2020-07-09 20:20:26 +0800

    add commnet for GenerateParentheses

--- src/com/audi/leetcode/recursion/GenerateParentheses.java
+++ src/com/audi/leetcode/recursion/GenerateParentheses.java
@@ -14,25 +14,29 @@ public class GenerateParentheses {
 
     public List<String> generateParenthesis(int n) {
         ArrayList<String> ret = new ArrayList<String>();
-        if (n <= 0)
+        if (n <= 0) {
             return ret;
+        }
+        // 递归调用
         dfs(n, n, "", ret);
         return ret;
     }
 
     private void dfs(int left, int right, String s, ArrayList<String> ret) {
-        if (left > right)
-            return;
-        else if (left == 0 && right == 0) {
+        // 左右括号数量相等  且左右括号都已经用完
+        if (left == 0 && right == 0) {
             ret.add(s);
             return;
         }
+        // 先加左括号
         if (left > 0) {
             dfs(left - 1, right, s + "(", ret);
         }
-        if (right > 0) {
+        // 再加右括号
+        if (left < right) {
             dfs(left, right - 1, s + ")", ret);
         }
+
     }
 
     public static void main(String[] args) {
