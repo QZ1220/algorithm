880c3dccca8af05a1f57dc33339cd1d1b55cd846
wangquanzhou <audi.car@qq.com>
2020-07-31 19:34:26 +0800

    add soluton for quick sort

--- src/com/audi/sort/QuickSort.java
+++ src/com/audi/sort/QuickSort.java
@@ -1,10 +1,11 @@
 package com.audi.sort;
 
 
-import java.util.Arrays;
-
 /**
  * https://developer.51cto.com/art/201403/430986.htm
+ * <p>
+ * https://github.com/AudiVehicle/learn/blob/master/2018%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0%E4%B8%80.md#java%E5%AE%9E%E7%8E%B0%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F%E5%92%8C%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F
+ * <p>
  * 快速排序
  *
  * @author: WangQuanzhou
