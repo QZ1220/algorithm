705ad8b2c1449f05dec3dee3f313e7e422372155
wangquanzhou <audi.car@qq.com>
2020-08-02 15:12:38 +0800

    add revrse node

--- src/com/audi/list/ReverseLinkedList.java
+++ src/com/audi/list/ReverseLinkedList.java
@@ -15,21 +15,38 @@ public class ReverseLinkedList {
             return head;
         }
 
-        ListNode dummyNode = new ListNode();
-        dummyNode.next = head;
-
-        while (null != head.next) {
-
+        // 注意这里初始为null
+        ListNode newHead = null;
+        ListNode oldHead = head;
+        while (null != oldHead) {
+            ListNode next = oldHead.next;
+            oldHead.next = newHead;
+            newHead = oldHead;
+            oldHead = next;
         }
-
+        return newHead;
     }
 
     public static void main(String[] args) {
+        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
 
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        ListNode head = reverseLinkedList.reverseList(node1);
+        while (null != head) {
+            System.out.println(head.getVal());
+            head = head.next;
+        }
     }
 
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
@@ -43,6 +60,10 @@ public class ReverseLinkedList {
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+
+        public int getVal() {
+            return this.val;
         }
     }
 }
