e2721f227461c14c11b94c0f6091f551c14e375e
wangquanzhou <audi.car@qq.com>
2020-08-08 11:17:05 +0800

    add solution for LinkedListCycle

--- src/com/audi/list/LinkedListCycle.java
+++ src/com/audi/list/LinkedListCycle.java
@@ -1,18 +1,16 @@
 package com.audi.list;
 
 /**
- * https://leetcode.com/problems/linked-list-cycle-ii/
+ * https://leetcode.com/problems/linked-list-cycle/
  * <p>
- * 求链表有没有环，有的话需要返回环的起始位置
+ * 求链表有没有环
  * <p>
  * 典型的可以使用快慢指针来解决这个问题
- * <p>
- * 或者也可以使用set来解决，但是需要申请额外的内存空间
  *
  * @author WangQuanzhou
  * @date 2020-08-08
  */
-public class LinkedListCycleII {
+public class LinkedListCycle {
 
     static class ListNode {
         int val;
@@ -25,46 +23,32 @@ public class LinkedListCycleII {
     }
 
     /**
-     * 检测链表是否有环，有环则返回环的起始节点
+     * 检测链表是否有环
      *
      * @param head
      * @return
      */
-    public ListNode detectCycle(ListNode head) {
+    public boolean hasCycle(ListNode head) {
         // 链表为空或者链表大小为1，则不可能有环
         if (null == head || null == head.next) {
-            return null;
+            return Boolean.FALSE;
         }
 
         ListNode slow = head;
         ListNode fast = head;
-        ListNode meet = null;
 
         while (slow != null && fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                // 相遇的位置不一定就是相遇的起始位置  这一点需要注意
-                meet = slow;
-                break;
+                return Boolean.TRUE;
             }
         }
-
-
-        slow = head;
-        // 下面计算相遇的起点位置 借助了一定的数学原理 具体可以参考这张图./list_cycleII.png
-        while (slow != null && meet != null) {
-            if (slow == meet) {
-                return slow;
-            }
-            slow = slow.next;
-            meet = meet.next;
-        }
-        return null;
+        return Boolean.FALSE;
     }
 
     public static void main(String[] args) {
-        LinkedListCycleII listCycleII = new LinkedListCycleII();
+        LinkedListCycle listCycleII = new LinkedListCycle();
         ListNode node1 = new ListNode(3);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(0);
@@ -73,8 +57,8 @@ public class LinkedListCycleII {
         node2.next = node3;
         node3.next = node4;
         node4.next = node2;
-        ListNode listNode = listCycleII.detectCycle(node1);
-        System.out.println(listNode.val);
+        boolean hasCycle = listCycleII.hasCycle(node1);
+        System.out.println(hasCycle);
     }
 
 }
