c5fa10611eaf17701eb39d40f575c5b5d66d55fb
wangquanzhou <audi.car@qq.com>
2020-08-09 17:16:02 +0800

    add BFS for NumberofIslands

--- src/com/audi/leetcode/search/NumberofIslands.java
+++ src/com/audi/leetcode/search/NumberofIslands.java
@@ -1,11 +1,18 @@
 package com.audi.leetcode.search;
 
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/number-of-islands/
  * <p>
  * 求小岛数量
  * <p>
  * 可以使用深度优先搜索  也可以使用宽度优先搜索
+ * <p>
+ * 从leetcode提交的结果来看  深度优先耗时略少于广度优先搜索
  *
  * @author WangQuanzhou
  * @date 2020-08-09
@@ -54,7 +61,8 @@ public class NumberofIslands {
             for (int j = 0; j < columns; j++) {
                 // 如果当前位置是陆地 且没有被搜索过
                 if (grid[i][j] == LAND && mark[i][j] == WATER) {
-                    DFS(grid, mark, i, j, rows, columns);
+//                    DFS(grid, mark, i, j, rows, columns);
+                    BFS(grid, mark, i, j, rows, columns);
                     count += 1;
                 }
             }
@@ -97,13 +105,62 @@ public class NumberofIslands {
 
             // 如果mark矩阵的newX，newY位置未被便利过  且  grid矩阵的该位置是陆地  那么就进行进行搜索
             if (mark[newX][newY] == WATER && grid[newX][newY] == LAND) {
-                DFS(grid, mark, newX, newY, rows, columns);
+                BFS(grid, mark, newX, newY, rows, columns);
             }
         }
     }
 
-    private void BFS(char[][] grid, char[][] mark, int x, int y) {
+    /**
+     * 宽度优先搜索
+     * <p>
+     * 每遍历一个节点，使用队列保存当前位置的上下左右节点，然后循环出队  直至队列为空为止
+     * <p>
+     * 注意这不是一个递归的过程  而是一个循环出队的过程
+     * <p>
+     * 循环的过程中需要更新mark矩阵
+     *
+     * @param grid    初始的grid
+     * @param mark    根据grid更新mark
+     * @param x       横坐标
+     * @param y       纵坐标
+     * @param rows    grid数组的行数
+     * @param columns grid数组的列数
+     */
+    private void BFS(char[][] grid, char[][] mark, int x, int y, int rows, int columns) {
+        if (mark[x][y] == LAND) {
+            return;
+        }
 
+        // 使用队列保存当前节点的上下左右节点
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+
+        // 更新mark矩阵
+        mark[x][y] = LAND;
+
+        queue.add(new Pair<>(x, y));
+
+        while (!queue.isEmpty()) {
+            // 出队
+            Pair<Integer, Integer> pair = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                // 注意下面的对新坐标的计算 一定要新指定一个变量名称  否则下面的continue语句会遍历不到4个方向
+                int newX = pair.getKey() + dx[i];
+                int newY = pair.getValue() + dy[i];
+                // 如果超出边界  直接返回
+                if (newX < 0 || newY < 0 || newX >= rows || newY >= columns) {
+                    continue;
+                }
+
+                // 如果mark矩阵的newX，newY位置未被便利过  且  grid矩阵的该位置是陆地  那么就进行进行搜索
+                if (mark[newX][newY] == WATER && grid[newX][newY] == LAND) {
+                    // 新元素入队
+                    queue.offer(new Pair<>(newX, newY));
+                    // 更新mark矩阵
+                    mark[newX][newY] = LAND;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -119,7 +176,7 @@ public class NumberofIslands {
 
 
         NumberofIslands numberofIslands = new NumberofIslands();
-//        System.out.println(numberofIslands.numIslands(grid));
-        System.out.println(numberofIslands.numIslands(new char[0][0]));
+        System.out.println(numberofIslands.numIslands(grid));
+//        System.out.println(numberofIslands.numIslands(new char[0][0]));
     }
 }
