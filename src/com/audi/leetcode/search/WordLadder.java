c931be10d2549dc3dcffa91f3585393da47c7ffd
wangquanzhou <audi.car@qq.com>
2020-08-09 18:22:02 +0800

    add WordLadder

---
+++ src/com/audi/leetcode/search/WordLadder.java
@@ -0,0 +1,25 @@
+package com.audi.leetcode.search;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/word-ladder/
+ * <p>
+ * 题目希望借助一些中间单词，将一个单词转换为另外一个单词，两两转换之间只能变动一个字符
+ * <p>
+ * 如果借助中间单词转换不了，就返回0
+ *
+ * @author WangQuanzhou
+ * @date 2020-08-09
+ */
+public class WordLadder {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
