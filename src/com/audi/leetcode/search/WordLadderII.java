1046423c91c919adbb7122b00f602ea090e7dbd6
wangquanzhou <audi.car@qq.com>
2020-08-15 09:47:32 +0800

    WordLadderII

---
+++ src/com/audi/leetcode/search/WordLadderII.java
@@ -0,0 +1,27 @@
+package com.audi.leetcode.search;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/word-ladder-ii/
+ * <p>
+ * 这道题是 https://leetcode.com/problems/word-ladder/ 的升级版
+ * <p>
+ * 需要求出所有的最短路径（不能重复）
+ * <p>
+ * 本题需要注意beginWord初始时在不在wordList中的情况，否则求出的结果会有重复的情况出现
+ *
+ * @author WangQuanzhou
+ * @date 2020-08-15
+ */
+public class WordLadderII {
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
