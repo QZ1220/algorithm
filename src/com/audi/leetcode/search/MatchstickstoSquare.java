311ad722e41d5ff911237623328783d7c4efa9c5
audi <audi.car@qq.com>
2020-08-22 10:52:45 +0800

    add MatchstickstoSquare

---
+++ src/com/audi/leetcode/search/MatchstickstoSquare.java
@@ -0,0 +1,13 @@
+package com.audi.leetcode.search;
+
+
+/**
+ * https://leetcode.com/problems/matchsticks-to-square/
+ * <p>
+ * 使用n个不通长度的火柴棍，能否围成一个正方形
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/8/22 10:51
+ */
+public class MatchstickstoSquare {
+}
