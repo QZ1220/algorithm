569ae17d07cfc9869a2541337a8078746cb2064a
wangquanzhou <audi.car@qq.com>
2020-09-17 19:55:30 +0800

    add SwiminRisingWater problem

---
+++ src/com/audi/leetcode/search/SwiminRisingWater.java
@@ -0,0 +1,24 @@
+package com.audi.leetcode.search;
+
+/**
+ * https://leetcode.com/problems/swim-in-rising-water/
+ * <p>
+ * N*N的二维框内，grid[i][j]表示（i，j）位置的高度，你可以从上下左右四个方向游动（移动的时间可以忽略），不能游出二维框。
+ * <p>
+ * 与此同时，天上也在一直下雨，t时刻，雨水的高度也是t，问需要多久，可以从（0，0）位置游到（N-1,N-1）位置
+ * <p>
+ * 典型的搜索问题
+ *
+ * @author WangQuanzhou
+ * @date 2020-09-17
+ */
+public class SwiminRisingWater {
+
+    public int swimInWater(int[][] grid) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
