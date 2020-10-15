f57e4b7e2ce76de8b389fab9c356cd2be4f898df
audi <audi.car@qq.com>
2020-10-16 07:59:33 +0800

    add leetcode 1036

---
+++ src/com/audi/leetcode/search/EscapeaLargeMaze.java
@@ -0,0 +1,29 @@
+package com.audi.leetcode.search;
+
+
+/**
+ * https://leetcode.com/problems/escape-a-large-maze/
+ * <p>
+ * <p>
+ * In a 1 million by 1 million grid, the coordinates of each grid square are (x, y) with 0 <= x, y < 10^6.
+ * <p>
+ * We start at the source square and want to reach the target square.  Each move, we can walk to a 4-directionally adjacent square in the grid that isn't in the given list of blocked squares.
+ * <p>
+ * Return true if and only if it is possible to reach the target square through a sequence of moves.
+ * <p>
+ * <p>
+ * 这题和https://leetcode.com/problems/number-of-islands/类似  但是这题的搜索数量级要大一些
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/10/16 7:55
+ */
+public class EscapeaLargeMaze {
+
+    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
