59f05243597ab3712e8d56f9783482a9e09efaec
audi <audi.car@qq.com>
2020-10-19 22:02:08 +0800

    add Matrix01

---
+++ src/com/audi/leetcode/search/Matrix01.java
@@ -0,0 +1,48 @@
+package com.audi.leetcode.search;
+
+
+/**
+ * https://leetcode.com/problems/01-matrix/
+ * <p>
+ * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+ * <p>
+ * The distance between two adjacent cells is 1.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * [[0,0,0],
+ * [0,1,0],
+ * [0,0,0]]
+ * <p>
+ * Output:
+ * [[0,0,0],
+ * [0,1,0],
+ * [0,0,0]]
+ * Example 2:
+ * <p>
+ * Input:
+ * [[0,0,0],
+ * [0,1,0],
+ * [1,1,1]]
+ * <p>
+ * Output:
+ * [[0,0,0],
+ * [0,1,0],
+ * [1,2,1]]
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/10/19 21:57
+ */
+public class Matrix01 {
+
+    public int[][] updateMatrix(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
