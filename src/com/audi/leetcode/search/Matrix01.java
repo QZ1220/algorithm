c1a107e57c3cc959d1265a0d485eb5848523a42b
wangquanzhou <audi.car@qq.com>
2020-10-21 20:51:49 +0800

    add solution for Matrix01

--- src/com/audi/leetcode/search/Matrix01.java
+++ src/com/audi/leetcode/search/Matrix01.java
@@ -1,6 +1,8 @@
 package com.audi.leetcode.search;
 
 
+import com.audi.heap.MaxHeap;
+
 /**
  * https://leetcode.com/problems/01-matrix/
  * <p>
@@ -39,10 +41,103 @@ package com.audi.leetcode.search;
 public class Matrix01 {
 
     public int[][] updateMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length < 2) {
+            return matrix;
+        }
+        // 矩阵大小
+        int N = matrix.length;
+        // int初始化默认为0
+        int[][] distance = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (matrix[i][j] == 0) {
+                    continue;
+                }
+
+                int left = j - 1;
+                int right = j + 1;
+                int up = i - 1;
+                int down = i + 1;
+                int zeroDis = N - 1;
+
+                if (left >= 0) {
+                    int leftZeroDis = 1;
+                    while (left >= 0) {
+                        if (matrix[i][left] == 0) {
+                            break;
+                        }
+                        leftZeroDis++;
+                        left--;
+                    }
+                    zeroDis = zeroDis > leftZeroDis ? leftZeroDis : zeroDis;
+                }
+
+                if (right < N) {
+                    int rightZeroDis = 1;
+                    while (right < N) {
+                        if (matrix[i][right] == 0) {
+                            break;
+                        }
+                        rightZeroDis++;
+                        right++;
+                    }
+                    zeroDis = zeroDis > rightZeroDis ? rightZeroDis : zeroDis;
+                }
+
+                if (up >= 0) {
+                    int upZeroDis = 1;
+                    while (up >= 0) {
+                        if (matrix[up][j] == 0) {
+                            break;
+                        }
+                        upZeroDis++;
+                        up--;
+                    }
+                    zeroDis = zeroDis > upZeroDis ? upZeroDis : zeroDis;
+                }
+
+
+                if (down < N) {
+                    int downZeroDis = 1;
+                    while (down < N) {
+                        if (matrix[down][j] == 0) {
+                            break;
+                        }
+                        downZeroDis++;
+                        down++;
+                    }
+                    zeroDis = zeroDis > downZeroDis ? downZeroDis : zeroDis;
+                }
+
+                distance[i][j] = zeroDis;
+            }
+        }
+        return distance;
 
     }
 
     public static void main(String[] args) {
+        int[][] matrix = {
+                {
+                        1, 1, 0
+                },
+                {
+                        0, 1, 0
+                },
+                {
+                        1, 1, 1
+                }
+        };
 
+        Matrix01 matrix01 = new Matrix01();
+        int[][] dis = matrix01.updateMatrix(matrix);
+        int length = dis.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(dis[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
