1d79c8247743be6a30c4ee5f159f5f2d235f4808
wangquanzhou <audi.car@qq.com>
2020-10-30 20:02:09 +0800

    add leetcode 3

--- src/com/audi/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/audi/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,14 @@
 package com.audi.leetcode.str;
 
-import com.audi.bst.Main;
-
 /**
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * <p>
+ * 思路：
+ * 使用LinkedHashMap<Char,List<Integer>>的结构  key为字符串中的字符  Value为字符串出现的位置
+ * <p>
+ * 如果所有的Value的长度都为1 那么整个字符串都没有重复字符
+ * <p>
+ * 否则从前往后，去除Value的值，从两个位置中间获取字符串元素
  *
  * @author WangQuanzhou
  * @date 2020-10-30
