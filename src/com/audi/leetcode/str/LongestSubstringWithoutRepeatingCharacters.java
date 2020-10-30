128e0cbd8b9bb8c39ee1f857bd8ecc23f20175cf
wangquanzhou <audi.car@qq.com>
2020-10-30 19:56:49 +0800

    add leetcode 3

---
+++ src/com/audi/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package com.audi.leetcode.str;
+
+import com.audi.bst.Main;
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * @author WangQuanzhou
+ * @date 2020-10-30
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
