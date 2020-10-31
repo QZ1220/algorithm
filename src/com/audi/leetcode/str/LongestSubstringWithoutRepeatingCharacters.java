372cc623377ecf1a0f66e5cc5b6cdc7e021600f8
audi <audi.car@qq.com>
2020-10-31 14:41:47 +0800

    add bad solution for leetcode 3

--- src/com/audi/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/audi/leetcode/str/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,9 @@
 package com.audi.leetcode.str;
 
+import org.omg.Messaging.SyncScopeHelper;
+
+import java.util.*;
+
 /**
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
  * <p>
@@ -16,11 +20,69 @@ package com.audi.leetcode.str;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
+        if (null == s) {
+            return 0;
+        }
+        int length = s.length();
+        if (length < 2) {
+            return length;
+        }
 
+        Boolean unique = Boolean.TRUE;
+
+        // 定义有序hashMap  默认按照插入顺序排序
+        Map<Character, List<Integer>> map = new LinkedHashMap<>();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.get(c).add(i);
+                unique = Boolean.FALSE;
+                continue;
+            }
+            List<Integer> list = new ArrayList<>();
+            list.add(i);
+            map.put(c, list);
+        }
+
+        if (unique) {
+            return length;
+        }
+
+        int maxlength = 0;
+        for (Character character : map.keySet()) {
+            List<Integer> list = map.get(character);
+            if (list.size() < 2) {
+                continue;
+            }
+            maxlength = max(list, length);
+        }
+        return maxlength;
+    }
+
+    private int max(List<Integer> list, int length) {
+        int size = list.size();
+        int leftMax = list.get(0) + 1;
+        int rightMax = length - list.get(size - 1);
+        int midMax = 0;
+
+        for (int i = size - 1; i > 0; i--) {
+            int distance = list.get(i) - list.get(i - 1);
+            if (midMax < distance) {
+                midMax = distance;
+            }
+        }
+        return leftMax > midMax ? (leftMax > rightMax ? leftMax : rightMax) : (midMax > rightMax ? midMax : rightMax);
     }
 
 
     public static void main(String[] args) {
+
+//        String s = "pwwkew";
+//        String s = "";
+//        String s = "cdd";
+        String s = "abcb";
+        LongestSubstringWithoutRepeatingCharacters characters = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(characters.lengthOfLongestSubstring(s));
 
     }
 }
