f05b8cce919b7eadcccef962a21679e487a10f63
audi <audi.car@qq.com>
2020-11-01 15:41:05 +0800

    add leetcode 5

---
+++ src/com/audi/leetcode/str/LongestPalindromicSubstring.java
@@ -0,0 +1,21 @@
+package com.audi.leetcode.str;
+
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ * <p>
+ * 参考链接：https://www.jianshu.com/p/a7741619dd58
+ * https://www.cxyxiaowu.com/2869.html
+ *
+ * @author: WangQuanzhou
+ * @date: 2020/11/1 15:39
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
