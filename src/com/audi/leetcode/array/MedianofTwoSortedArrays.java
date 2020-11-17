b2cde3b0479a3365e30ef5d8f031d13e5b5f5710
audi <audi.car@qq.com>
2020-11-17 22:07:46 +0800

    add commne t for leetcode 4

--- src/com/audi/leetcode/array/MedianofTwoSortedArrays.java
+++ src/com/audi/leetcode/array/MedianofTwoSortedArrays.java
@@ -12,7 +12,45 @@ package com.audi.leetcode.array;
  */
 public class MedianofTwoSortedArrays {
 
-    public static void main(String[] args) {
+    /**
+     * Constraints:
+     * <p>
+     * nums1.length == m
+     * nums2.length == n
+     * 0 <= m <= 1000
+     * 0 <= n <= 1000
+     * 1 <= m + n <= 2000
+     * -106 <= nums1[i], nums2[i] <= 106
+     * <p>
+     * The overall run time complexity should be O(log (m+n))
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m < 1) {
+            if (n % 2 != 0) {
+                return Double.valueOf(nums2[n / 2]);
+            }
+            return Double.valueOf((nums2[n / 2] + nums2[(n / 2) - 1] + 0.0) / 2);
+        }
 
+        if (n < 1) {
+            if (m % 2 != 0) {
+                return Double.valueOf(nums2[m / 2]);
+            }
+            return Double.valueOf((nums2[m / 2] + nums2[(m / 2) - 1] + 0.0) / 2);
+        }
+
+        // fixme 是否需要合并数组
+        return m;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1 % 2);
+        System.out.println(1 / (2 + 0.0));
     }
 }
