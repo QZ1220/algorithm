4abe78e8aa895aa66c0cbe11511dfbee7343cd70
王全洲 <wangquanzhou@t4f-mbp-wqz.local>
2021-08-04 22:21:03 +0800

    add leetcode 100 and 101

---
+++ src/com/audi/leetcode/Tree/SameTree.java
@@ -0,0 +1,50 @@
+package com.audi.leetcode.Tree;
+
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ * <p>
+ * 判断两颗二叉树是否相等
+ *
+ * @author: WangQuanzhou
+ * @date: 2021-08-04 10:18 PM
+ */
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+    public static void main(String[] args) {
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/com/audi/leetcode/Tree/SymmetricTree.java
@@ -0,0 +1,51 @@
+package com.audi.leetcode.Tree;
+
+
+/**
+ * https://leetcode.com/problems/symmetric-tree/
+ * <p>
+ * 对称二叉树
+ * <p>
+ * 递归判断
+ *
+ * @author: WangQuanzhou
+ * @date: 2021-08-04 10:18 PM
+ */
+public class SymmetricTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
