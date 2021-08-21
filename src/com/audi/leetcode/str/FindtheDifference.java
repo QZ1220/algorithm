5d5f01440295f47110f865b0168028fb5864b351
wangquanzhou <audi.car@qq.com>
2021-08-21 19:43:07 +0800

    add leetcode 389

---
+++ src/com/audi/leetcode/str/FindtheDifference.java
@@ -0,0 +1,71 @@
+package com.audi.leetcode.str;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/find-the-difference/
+ * <p>
+ * 找出两个字符串的最开始有差异的位置，当然，这两个字符串也只有这一个差异，其余位置都相同
+ * <p>
+ * 题意理解错了，字符串s和t，t是由s的字符打乱以后组成的新的字符串，并且新的字符串还在随机位置增加了一个字符，使用map统计字符
+ *
+ * @author: WangQuanzhou
+ * @date: 2021-08-21 7:18 PM
+ */
+public class FindtheDifference {
+
+    public char findTheDifference(String s, String t) {
+        if (null == s || s.length() < 1) {
+            return t.charAt(0);
+        }
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                return t.charAt(i);
+            }
+        }
+        return t.charAt(length);
+    }
+
+    public char findTheDifference2(String s, String t) {
+        if (null == s || s.length() < 1) {
+            return t.charAt(0);
+        }
+        Map<Character, Integer> maps = new HashMap<>(32);
+        Map<Character, Integer> mapt = new HashMap<>(32);
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            put(c1, maps);
+            put(c2, mapt);
+        }
+        char c = t.charAt(len);
+        put(c, mapt);
+
+        for (Character ch : mapt.keySet()) {
+            if (!maps.containsKey(ch) || (maps.get(ch) != mapt.get(ch))) {
+                return ch;
+            }
+        }
+        return c;
+    }
+
+    private void put(Character c, Map<Character, Integer> map) {
+        if (map.containsKey(c)) {
+            map.put(c, map.get(c) + 1);
+        } else {
+            map.put(c, 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor";
+        String t = "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj";
+        FindtheDifference difference = new FindtheDifference();
+        char c = difference.findTheDifference2(s, t);
+        System.out.println(c);
+    }
+}
