cde94ebe2eda369eb4c87b5e56d3a6e28ddb1f39
wangquanzhou <audi.car@qq.com>
2021-10-19 13:18:14 +0800

    add leetcode 237

--- src/com/audi/leetcode/list/DeleteNodeinaLinkedList.java
+++ src/com/audi/leetcode/list/DeleteNodeinaLinkedList.java
@@ -4,7 +4,9 @@ package com.audi.leetcode.list;
 /**
  * https://leetcode.com/problems/delete-node-in-a-linked-list/
  * <p>
- * 删除链表中的一个元素
+ * 删除链表中的一个元素，题设限定了删除的元素不是尾结点
+ * <p>
+ * 需要删除的元素已经直接给出了，但是链表的头节点没有给出
  *
  * @author: WangQuanzhou
  * @date: 2021-10-19 9:27 AM
@@ -12,7 +14,8 @@ package com.audi.leetcode.list;
 public class DeleteNodeinaLinkedList {
 
     public void deleteNode(ListNode node) {
-
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
     public static void main(String[] args) {
