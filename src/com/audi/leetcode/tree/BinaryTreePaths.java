1328dc0f9401f124b9de3222f841d09e75d013cb
Audi <audi.car@qq.com>
2023-03-17 12:29:51 +0800

    add leetcode 257

---
+++ src/com/audi/leetcode/tree/BinaryTreePaths.java
@@ -0,0 +1,51 @@
+package com.audi.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+
+/**
+ * https://leetcode.com/problems/binary-tree-paths/
+ * <p>
+ * 求二叉树的所有到叶子节点的路径
+ *
+ * @author : wangquanzhou
+ * @date : 2023/3/17 11:36
+ */
+public class BinaryTreePaths {
+
+    private static final String SEPARATOR = "->";
+
+    /**
+     * 使用递归求解
+     *
+     * @param root
+     * @return
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new LinkedList<>();
+        travel(root, "", paths);
+        return paths;
+    }
+
+    private void travel(TreeNode root, String path, List<String> paths) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            path = path + root.val;
+            paths.add(path);
+            return;
+        }
+        path = path + root.val + SEPARATOR;
+        travel(root.left, path, paths);
+        travel(root.right, path, paths);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
