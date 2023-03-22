8ca637e1eee7ace951be800244ae4709dab8d956
Audi <audi.car@qq.com>
2023-03-22 22:35:51 +0800

    add leetcode

--- src/com/audi/leetcode/greed/BestTimetoBuyandSellStock.java
+++ src/com/audi/leetcode/greed/BestTimetoBuyandSellStock.java
@@ -2,9 +2,9 @@ package com.audi.leetcode.greed;
 
 /**
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan&id=level-1
- *
+ * <p>
  * 给定一个整形数组，每个元素代表当天的股价，你需要在其中某一天买入，后面的某一天卖出，目的是要获取最大的利润，也是卖出-买入的差价最大
- *
+ * <p>
  * 从数组的末尾往前遍历，求每个元素位置，其右边的最大值，最大值与本位置元素的差值，求一个最大值，即可求解。
  *
  * @author : wangquanzhou
@@ -13,7 +13,20 @@ package com.audi.leetcode.greed;
 public class BestTimetoBuyandSellStock {
 
     public int maxProfit(int[] prices) {
-
+        int len = prices.length;
+        // 其实，这里也并不是非要使用数组，使用一个遍历存储即可
+        int[] rightMax = new int[len];
+        rightMax[len - 1] = prices[len - 1];
+        int maxProfit = 0;
+        for (int i = len - 2; i >= 0; i--) {
+            if (prices[i] > rightMax[i + 1]) {
+                rightMax[i] = prices[i];
+            } else {
+                rightMax[i] = rightMax[i + 1];
+            }
+            maxProfit = (rightMax[i] - prices[i]) > maxProfit ? (rightMax[i] - prices[i]) : maxProfit;
+        }
+        return maxProfit;
     }
 
     public static void main(String[] args) {
