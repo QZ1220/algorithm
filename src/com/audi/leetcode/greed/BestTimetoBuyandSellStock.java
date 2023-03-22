7a264cf442f8caafd3ff93cb0dd9a555eaa7227b
Audi <audi.car@qq.com>
2023-03-22 22:26:53 +0800

    add leetcode 121

---
+++ src/com/audi/leetcode/greed/BestTimetoBuyandSellStock.java
@@ -0,0 +1,22 @@
+package com.audi.leetcode.greed;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan&id=level-1
+ *
+ * 给定一个整形数组，每个元素代表当天的股价，你需要在其中某一天买入，后面的某一天卖出，目的是要获取最大的利润，也是卖出-买入的差价最大
+ *
+ * 从数组的末尾往前遍历，求每个元素位置，其右边的最大值，最大值与本位置元素的差值，求一个最大值，即可求解。
+ *
+ * @author : wangquanzhou
+ * @date : 2023/3/22 22:14
+ */
+public class BestTimetoBuyandSellStock {
+
+    public int maxProfit(int[] prices) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
