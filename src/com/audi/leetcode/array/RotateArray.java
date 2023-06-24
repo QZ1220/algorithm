f3c22c16ab482c6fb5cca5ebfcb2f22bac9e55fb
Audi <audi.car@qq.com>
2023-06-24 23:15:31 +0800

    add leetcode 189

---
+++ src/com/audi/leetcode/array/RotateArray.java
@@ -0,0 +1,25 @@
+package com.audi.leetcode.array;
+
+/**
+ * https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+ * <p>
+ * 旋转数组，eg：
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * <p>
+ * Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ * @author : wangquanzhou
+ * @date : 2023/6/24 23:13
+ */
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
