cbe8e678563eafc86887cbab6429c3823dbae5ca
Audi <audi.car@qq.com>
2023-07-11 22:49:45 +0800

    add solution

--- src/com/audi/other/ListDemo.java
+++ src/com/audi/other/ListDemo.java
@@ -1,5 +1,7 @@
 package com.audi.other;
 
+import java.util.*;
+
 /**
  * list relevant algorithm
  *
@@ -8,7 +10,7 @@ package com.audi.other;
  */
 public class ListDemo {
     public ListNode partition(ListNode head, int x) {
-        if (null==head||null==head.next){
+        if (null == head || null == head.next) {
             return head;
         }
         ListNode leftDummyHead = new ListNode(-1);
@@ -26,16 +28,54 @@ public class ListDemo {
             head = head.next;
         }
 
-        rightHead.next=null;
+        rightHead.next = null;
 
         // merge two list
-        leftHead.next=rightDummyHead.next;
+        leftHead.next = rightDummyHead.next;
         return leftDummyHead.next;
     }
 
+    public Node copyRandomList(Node head) {
+        if (null == head) {
+            return head;
+        }
+        List<Node> list = new ArrayList<>();
+        Map<Node, Integer> map = new HashMap<>();
 
+        Node ptr = head;
+        int i = 0;
+        while (null != ptr) {
+            list.add(new Node(ptr.val));
+            map.put(ptr, i);
+            ptr = ptr.next;
+            i++;
+        }
+
+        ptr = head;
+        // 在list末尾加一个null元素，可以避免处理链表尾部的情况
+        list.add(null);
+        i = 0;
+        while (null != ptr) {
+            list.get(i).next = list.get(i + 1);
+            if (null != ptr.random) {
+                list.get(i).random=list.get(map.get(ptr.random));
+            }
+            ptr=ptr.next;
+            i++;
+        }
+        return list.get(0);
+    }
 
     public static void main(String[] args) {
+        Node node0=new Node(1);
+        Node node1=new Node(2);
+        node0.next=node1;
+        node0.random=node1;
+        node1.random=node1;
+        ListDemo demo = new ListDemo();
+        Node node = demo.copyRandomList(node0);
+        System.out.println(node.val);
+
 
     }
 }
