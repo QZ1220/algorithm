1b99712e02d19f82aaeed16da4a74f0787155847
Audi <audi.car@qq.com>
2023-07-12 22:44:56 +0800

    add solution

--- src/com/audi/leetcode/list/SortList.java
+++ src/com/audi/leetcode/list/SortList.java
@@ -23,20 +23,22 @@ public class SortList {
     }
 
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
+        if (null == head || null == head.next) {
             return head;
         }
-        // 使用快慢指针的思想，当fast走到链表末尾时，slow正好走到链表的中间位置
-        // pre指针的作用是断开前后子链表的链接  否则递归调用会栈溢出
-        ListNode fast = head, slow = head, pre = head;
-        while (fast != null && fast.next != null) {
-            pre = slow;
+
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode slow = dummyNode;
+        ListNode fast = dummyNode;
+        while (null != fast && null != fast.next) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        // 断开前后链表之前的链接关系
-        pre.next = null;
-        return merge(sortList(head), sortList(slow));
+        ListNode head2 = slow.next;
+        slow.next = null;
+
+        return merge(sortList(head), sortList(head2));
     }
 
     /**
