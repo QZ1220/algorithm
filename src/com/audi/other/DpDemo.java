958d6d4f9b28b3441b21719e6e7e16136678a2b8
Audi <audi.car@qq.com>
2023-07-15 12:03:07 +0800

    add solution

--- src/com/audi/other/DpDemo.java
+++ src/com/audi/other/DpDemo.java
@@ -137,6 +137,35 @@ public class DpDemo {
         return list.size();
     }
 
+    /**
+     * 二维矩阵的最小路径和
+     *
+     * 使用动态规划实现
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int H = grid.length;
+        int L = grid[0].length;
+
+        int[][] dp = new int[H][L];
+        dp[0][0]=grid[0][0];
+        for (int i = 1; i < L; i++) {
+            dp[0][i]=dp[0][i-1]+grid[0][i];
+        }
+        for (int i = 1; i < H; i++) {
+            dp[i][0]=dp[i-1][0]+grid[i][0];
+        }
+
+        for (int i = 1; i < H; i++) {
+            for (int j = 1; j < L; j++) {
+                dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[H-1][L-1];
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {4, 10, 4, 3, 8, 9};
