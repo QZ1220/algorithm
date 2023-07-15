e0678d5c27b875ece513ce0851636b2c87a29d70
Audi <audi.car@qq.com>
2023-07-15 11:52:03 +0800

    add solution for 300

--- src/com/audi/other/DpDemo.java
+++ src/com/audi/other/DpDemo.java
@@ -1,6 +1,7 @@
 package com.audi.other;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -87,6 +88,8 @@ public class DpDemo {
 
     /**
      * 最长上升子序列
+     * <p>
+     * 使用动态规划求解，时间复杂度O(N^2)
      *
      * @param nums
      * @return
@@ -94,20 +97,50 @@ public class DpDemo {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-        int LIS=1;
+        int LIS = 1;
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            LIS = Math.max(LIS,dp[i]);
+            LIS = Math.max(LIS, dp[i]);
         }
         return LIS;
     }
 
 
-    public static void main(String[] args) {
+    /**
+     * 借助栈的思想
+     * <p>
+     * 时间复杂度O(NlogN)
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        List<Integer> list = new LinkedList<>();
+        list.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > list.get(list.size() - 1)) {
+                list.add(nums[i]);
+            } else {
+                for (int j = 0; j < list.size(); j++) {
+                    // 下面的等号不能省去，考虑测试用例：{4, 10, 4, 3, 8, 9}
+                    if (nums[i] <= list.get(j)) {
+                        list.set(j, nums[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        return list.size();
+    }
 
+
+    public static void main(String[] args) {
+        int[] nums = {4, 10, 4, 3, 8, 9};
+        DpDemo dpDemo = new DpDemo();
+        System.out.println(dpDemo.lengthOfLIS2(nums));
     }
 }
