8de20b9d923829e50c98c1d45558d0a578865ae5
Audi <audi.car@qq.com>
2023-07-15 11:13:17 +0800

    add solution

--- src/com/audi/other/DpDemo.java
+++ src/com/audi/other/DpDemo.java
@@ -1,5 +1,9 @@
 package com.audi.other;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * dp algorithm
  *
@@ -16,6 +20,7 @@ public class DpDemo {
      * @return
      */
     public int coinChange(int[] coins, int amount) {
+        // dp[i]表示组成amount需要的最少的coin的个数
         int[] dp = new int[amount + 1];
         for (int i = 0; i < amount + 1; i++) {
             dp[i] = -1;
@@ -32,6 +37,57 @@ public class DpDemo {
         }
         return dp[amount];
     }
+
+
+    /**
+     * coin change II
+     * <p>
+     * Return the number of combinations that make up that amount.
+     * If that amount of money cannot be made up by any combination of the coins, return 0.
+     *
+     * @param amount
+     * @param coins
+     * @return
+     */
+//    public int change(int amount, int[] coins) {
+//        // dp[i] 表示组成金额i，可能的组合方式的个数
+//        int[] dp = new int[amount + 1];
+//        Arrays.fill(dp,-1);
+//        dp[0]=0;
+//
+//        for (int i = 1; i <= amount; i++) {
+//            for (int j = 0; j < coins.length; j++) {
+//                if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
+//                    dp[i]++;
+//                }
+//            }
+//        }
+//        return dp[amount];
+//    }
+
+
+     public int minimumTotal(List<List<Integer>> triangle) {
+
+         // 最大的行数
+         int H = triangle.size();
+         // 最大的列数
+         int L = triangle.get(H-1).size();
+
+         int[][] dp = new int[H][L];
+
+         // 初始化dp数组的最后一行
+         for (int i = 0; i < L; i++) {
+             dp[H-1][i]=triangle.get(H-1).get(i);
+         }
+
+         for (int i = H-2; i >=0 ; i--) {
+             for (int j = 0; j < triangle.get(i).size(); j++) {
+                 dp[i][j] = Math.min(dp[i+1][j],dp[i+1][j+1])+triangle.get(i).get(j);
+             }
+         }
+         return dp[0][0];
+    }
+
 
     public static void main(String[] args) {
 
