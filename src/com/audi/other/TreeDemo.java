102cd41bce77d085587604ae753bc0cdb5ed4c5b
Audi <audi.car@qq.com>
2023-07-16 09:57:26 +0800

    add code

--- src/com/audi/other/TreeDemo.java
+++ src/com/audi/other/TreeDemo.java
@@ -57,7 +57,42 @@ public class TreeDemo {
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * 前序遍历二叉树，并且输出二叉树的节点所在的层数，root节点为第0层
+     *
+     * @param root
+     * @param layer
+     */
+    public void preOrder(TreeNode root, int layer) {
+        if (root == null) {
+            return;
+        }
+        System.out.println("val= " + root.val + "  layer= " + layer);
+        preOrder(root.left, layer + 1);
+        preOrder(root.right, layer + 1);
+    }
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(0);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        root.left=node1;
+        root.right=node2;
+
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        node1.left = node3;
+        node1.right=node4;
+
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        node2.left=node5;
+        node2.right=node6;
+
+        TreeNode node7 = new TreeNode(7);
+        node3.left=node7;
+
+        TreeDemo treeDemo = new TreeDemo();
+        treeDemo.preOrder(root,0);
     }
 }
