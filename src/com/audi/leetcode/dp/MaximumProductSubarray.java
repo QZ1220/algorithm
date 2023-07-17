10a620c63930c7d87c732a360eb0dcb734b64785
Audi <audi.car@qq.com>
2023-07-17 21:40:12 +0800

    add code

---
+++ src/com/audi/leetcode/dp/MaximumProductSubarray.java
@@ -0,0 +1,43 @@
+package com.audi.leetcode.dp;
+
+/**
+ * @author : wangquanzhou
+ * @date : 2023/7/17 21:34
+ */
+public class MaximumProductSubarray {
+
+    public int maxProduct(int[] nums) {
+        if (null == nums) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int maxRes = nums[0];
+        int maxP = nums[0];
+        int minP = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                maxP = Math.max(nums[i], nums[i] * maxP);
+                minP = Math.min(nums[i], nums[i] * minP);
+            } else if (nums[i] < 0) {
+                int tmp = maxP;
+                maxP = Math.max(nums[i], nums[i] * minP);
+                minP = Math.min(nums[i], nums[i] * tmp);
+            } else {
+                maxP = 0;
+                minP = 0;
+            }
+            maxRes = Math.max(maxP, maxRes);
+        }
+        return maxRes;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, -3, -2, -4};
+        MaximumProductSubarray maximumProductSubarray = new MaximumProductSubarray();
+        System.out.println(maximumProductSubarray.maxProduct(nums));
+    }
+}
