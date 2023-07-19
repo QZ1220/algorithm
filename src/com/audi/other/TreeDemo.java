2354c0732b72a1c8b414888411500532301a24b1
Audi <audi.car@qq.com>
2023-07-19 22:42:29 +0800

    solution

--- src/com/audi/other/TreeDemo.java
+++ src/com/audi/other/TreeDemo.java
@@ -292,20 +292,20 @@ public class TreeDemo {
 
     public int rob(TreeNode root) {
         int[] values = getValues(root);
-        return Math.max(values[0],values[1]);
+        return Math.max(values[0], values[1]);
     }
 
     /**
      * 自底向上寻找的思路
-     *
+     * <p>
      * 参考视频：
      * https://www.bilibili.com/video/BV15T411h7Dn/?spm_id_from=333.337.search-card.all.click&vd_source=d1530fb814268f770330143e24aaf1e6
      *
      * @param root
      * @return
      */
-    private int[] getValues(TreeNode root){
-        if (root==null){
+    private int[] getValues(TreeNode root) {
+        if (root == null) {
             return new int[2];
         }
 
@@ -313,9 +313,9 @@ public class TreeDemo {
         int[] rightValues = getValues(root.right);
         // values[0] 表示当前root节点要偷时，可以获得的最大值
         // values[1] 表示当前root节点不偷时，可以获得的最大值
-        int[] values =new int[2];
-        values[0] =root.val+leftValues[1]+rightValues[1];
-        values[1]= Math.max(leftValues[0],leftValues[1])+Math.max(rightValues[0],rightValues[1]);
+        int[] values = new int[2];
+        values[0] = root.val + leftValues[1] + rightValues[1];
+        values[1] = Math.max(leftValues[0], leftValues[1]) + Math.max(rightValues[0], rightValues[1]);
         return values;
     }
 
