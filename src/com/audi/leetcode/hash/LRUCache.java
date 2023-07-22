1f35f10dbeeac334d24015d41e4b60bdf22dad92
Audi <audi.car@qq.com>
2023-07-22 16:49:16 +0800

    add solution

--- src/com/audi/leetcode/hash/LRUCache.java
+++ src/com/audi/leetcode/hash/LRUCache.java
@@ -1,8 +1,6 @@
 package com.audi.leetcode.hash;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/lru-cache/
@@ -19,45 +17,44 @@ import java.util.Set;
 public class LRUCache {
 
     // LRU缓存
-    Map<Integer, Integer> cache;
-    int max_size = 0;
+    Map<Integer, Integer> map;
+    LinkedList<Integer> list;
+    int maxSize;
 
-    /**
-     * 初始化一个指定容量的LRUCache
-     *
-     * @param capacity cache容量
-     */
+
     public LRUCache(int capacity) {
-        cache = new LinkedHashMap<>(capacity, 1.0f, Boolean.TRUE);
-        max_size = capacity;
+        map = new HashMap<>(capacity);
+        list = new LinkedList<>();
+        maxSize = capacity;
     }
 
-    /**
-     * 从cache获取指定key的信息，如果没有则返回-1
-     *
-     * @param key
-     * @return
-     */
     public int get(int key) {
-        return null == cache.get(key) ? -1 : cache.get(key);
+        if (map.containsKey(key)) {
+            int index = list.indexOf(key);
+            list.remove(index);
+            list.addFirst(key);
+            return map.get(key);
+        }
+        return -1;
     }
 
-    /**
-     * 将键值对存入cache，
-     * 如果key存在就覆盖原来的value
-     * 如果不存在且cache容量未到capacity时，设置键值对到cache
-     * 如果不存在且cache容量已到capacity时，则清理cache中最少使用的键值对，并设置键值对到cache
-     *
-     * @param key
-     * @param value
-     */
     public void put(int key, int value) {
-        if (cache.size() >= max_size && !cache.containsKey(key)) {
-            Set<Integer> keySet = cache.keySet();
-            cache.remove(keySet.toArray()[0]);
+        if (map.containsKey(key)) {
+            int index = list.indexOf(key);
+            list.remove(index);
+            list.addFirst(key);
+            map.put(key,value);
+        } else {
+            if (list.size() >= maxSize) {
+                Integer tmp = list.get(list.size() - 1);
+                list.removeLast();
+                map.remove(tmp);
+            }
+            map.put(key, value);
+            list.addFirst(key);
         }
-        cache.put(key, value);
     }
+
 
     public static void main(String[] args) {
     }
