3297a57522f2a2196e844999bb2c328dfee7befb
Audi <audi.car@qq.com>
2023-07-29 15:11:23 +0800

    注意mid计算结果的细微差异

--- src/com/audi/leetcode/search/KthSmallestElementinaSortedMatrix.java
+++ src/com/audi/leetcode/search/KthSmallestElementinaSortedMatrix.java
@@ -26,7 +26,7 @@ public class KthSmallestElementinaSortedMatrix {
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < L; j++) {
                 queue.offer(matrix[i][j]);
-                if (queue.size()>k){
+                if (queue.size() > k) {
                     queue.poll();
                 }
             }
@@ -35,8 +35,64 @@ public class KthSmallestElementinaSortedMatrix {
         return queue.peek();
     }
 
+    /**
+     * 使用二分查找的思想解题
+     * <p>
+     * 解题思路：
+     * <p>
+     * 使用二分查找的思想，设定搜索的范围是从矩阵最小值到最大值。最小值是矩阵的左上角元素，最大值是矩阵的右下角元素。
+     * <p>
+     * 在每一次的二分查找中，计算矩阵中小于等于当前中间值的元素个数。具体地，从矩阵的左下角开始遍历，如果当前元素小于等于中间值，
+     * 就将当前所在列的元素个数加到计数器中，并向右移动一列；否则，向上移动一行。
+     * <p>
+     * 根据计数器的值与k的关系，缩小搜索范围。
+     * 如果计数器小于k，说明第k小的元素位于当前范围的右侧，将搜索范围缩小到大于中间值的部分；
+     * 如果计数器大于等于k，说明第k小的元素位于当前范围的左侧，将搜索范围缩小到小于等于中间值的部分。
+     * <p>
+     * 当搜索范围缩小到只有一个元素时，即左右指针相遇时，这个元素就是第k小的元素。
+     *
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthSmallest2(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0], right = matrix[n - 1][n - 1];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = countLessOrEqual(matrix, mid, n);
+
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private int countLessOrEqual(int[][] matrix, int target, int n) {
+        int count = 0;
+        int row = n - 1, col = 0; // 从左下角开始遍历
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= target) {
+                count += row + 1;
+                col++;
+            } else {
+                row--;
+            }
+        }
+
+        return count;
+    }
+
 
     public static void main(String[] args) {
-
+        int[][] nums = {{-5, -4}, {-5, -4}};
+        KthSmallestElementinaSortedMatrix sortedMatrix = new KthSmallestElementinaSortedMatrix();
+        System.out.println(sortedMatrix.kthSmallest2(nums, 2));
     }
 }
--- src/com/audi/other/SearchDemo.java
+++ src/com/audi/other/SearchDemo.java
@@ -140,10 +140,11 @@ public class SearchDemo {
     public boolean searchMatrix(int[][] matrix, int target) {
         int H = matrix.length;
         int L = matrix[0].length;
-        if (target < matrix[0][0] || target > matrix[H-1][L-1]) {
+        if (target < matrix[0][0] || target > matrix[H - 1][L - 1]) {
             return false;
         }
-        find(matrix, target, H-1, 0);
+        // 从矩阵的左下角开始搜索，左下角的元素为该行的最小值，该列的最大值
+        find(matrix, target, H - 1, 0);
         return flag;
     }
 
@@ -156,12 +157,78 @@ public class SearchDemo {
             flag = true;
             return;
         }
+        // 如果target小于当前位置元素，则说明应该向上移动一行
         if (matrix[x][y] > target) {
             find(matrix, target, x - 1, y);
         } else {
+            // 如果target大于当前位置元素，则说明应该向右移动一列
             find(matrix, target, x, y + 1);
         }
     }
+
+
+    /**
+     * 给定一个n*n的二维矩阵，矩阵的每一行都是从小到大排列，每一列也都是从小到大排列
+     * 求矩阵的第k小的元素
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int kthSmallest(int[][] nums, int k) {
+        int n = nums.length;
+        int left = nums[0][0];
+        int right = nums[n - 1][n - 1];
+        while (left < right) {
+            /**
+             * (left + right) / 2 和 left + (right - left) / 2 在大多数情况下是等价的，都可以用来计算两个数的中间值。
+             *
+             * 然而，在涉及整数除法时，两者有微小的差异。如果 (right - left) 是奇数，那么使用 left + (right - left) / 2 计算中间值会向下取整，
+             * 而 (left + right) / 2 会向上取整。这个差异可能会在一些特定情况下引起问题。
+             *
+             * 为了保证结果的准确性和一致性，在大多数情况下，我们建议使用 left + (right - left) / 2 来计算中间值。
+             * 这样可以确保得到正确的结果，并防止潜在的问题出现。
+             *
+             *
+             *
+             *
+             * 对于有符号数，left + (right - left) / 2 和 left + (right - left) >> 1 在大多数情况下是等价的。由于右移操作保留符号位，
+             * 所以可以正确处理有符号数的取整操作。
+             *
+             * 然而，需要注意的是如果 left 和 right 是负数，并且 (right - left) 是奇数，使用右移操作 (right - left) >> 1 可能导致舍入错误。
+             * 由于右移操作保留符号位，这的舍入错误可能会使得最终结果比预期的稍微偏小。
+             *
+             * 为了避免这种问题，在处理有符号数时，建议使用 left + (right - left) / 2 来计算中间值，以确保得到正确的结果。
+             * */
+//            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) >> 1;
+            int mid222 = (left + right) / 2;
+            System.out.println("mid = " + mid + " ---   mid222 = " + mid222);
+            if (countLessOrEqual(nums, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private int countLessOrEqual(int[][] nums, int target) {
+        int count = 0;
+        int H = nums.length - 1;
+        int L = 0;
+        // 行方向递减，列方向递增
+        while (H >= 0 && L < nums[0].length) {
+            if (nums[H][L] <= target) {
+                count = count + H + 1;
+                L++;
+            } else {
+                H--;
+            }
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
 //        char[][] grid = {
@@ -173,6 +240,10 @@ public class SearchDemo {
 //        SearchDemo searchDemo = new SearchDemo();
 //        int islands = searchDemo.numIslands(grid);
 //        System.out.println(islands);
+
+        int[][] nums = {{-5, -4}, {-5, -4}};
+        SearchDemo searchDemo = new SearchDemo();
+        System.out.println(searchDemo.kthSmallest(nums, 2));
 
     }
 }
