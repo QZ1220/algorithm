dc7c65a1dbb1cd66115a5a9b88563b63f41ab079
Audi <audi.car@qq.com>
2023-07-30 09:50:30 +0800

    half solution

--- src/com/audi/leetcode/search/KthSmallestinLexicographicalOrder.java
+++ src/com/audi/leetcode/search/KthSmallestinLexicographicalOrder.java
@@ -28,29 +28,75 @@ public class KthSmallestinLexicographicalOrder {
      * @param k
      * @return
      */
-    public int findKthNumber(int n, int k) {
+    public int findKthNumber2(int n, int k) {
         Queue<String> queue = new PriorityQueue<>(Comparator.comparing(String::valueOf).reversed());
         for (int i = 1; i <= n; i++) {
             queue.add(String.valueOf(i));
-            if (queue.size()>k){
+            if (queue.size() > k) {
                 queue.poll();
             }
         }
         return Integer.valueOf(queue.peek());
     }
 
-    public static void main(String[] args) {
-        Queue<String> queue = new PriorityQueue<>();
-        queue.add("11");
-        queue.add("10");
-        queue.add("1");
+    /**
+     * 借助堆的思想建立一个大小为k的堆   实际提交发现内存超限
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public int findKthNumber(int n, int k) {
 
-        while (!queue.isEmpty()) {
-            System.out.println(queue.poll());
+        String[] strs = new String[k];
+        for (int i = 0; i < k; i++) {
+            strs[i] = String.valueOf(i + 1);
         }
 
+        // 构建大顶堆
+        for (int i = k / 2 - 1; i >= 0; i--) {
+            heapify(strs, k, i);
+        }
+
+        for (int i = k; i < n; i++) {
+            if (String.valueOf(i+1).compareTo(strs[0]) > 0) {
+                continue;
+            }
+            strs[0] = String.valueOf(i+1);
+            heapify(strs, k, 0);
+        }
+        return Integer.valueOf(strs[0]);
+    }
+
+    /**
+     * 构建一个大顶堆
+     *
+     * @param strs
+     * @param n
+     * @param i
+     */
+    private void heapify(String[] strs, int n, int i) {
+        int parent = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && strs[left].compareTo(strs[parent]) > 0) {
+            parent = left;
+        }
+        if (right < n && strs[right].compareTo(strs[parent]) > 0) {
+            parent = right;
+        }
+        if (parent != i) {
+            String tmp = strs[i];
+            strs[i] = strs[parent];
+            strs[parent] = tmp;
+            heapify(strs, n, parent);
+        }
+    }
+
+
+    public static void main(String[] args) {
         KthSmallestinLexicographicalOrder lexicographicalOrder = new KthSmallestinLexicographicalOrder();
-        System.out.println(lexicographicalOrder.findKthNumber(13, 2));
-        System.out.println(lexicographicalOrder.findKthNumber(1, 1));
+        int number = lexicographicalOrder.findKthNumber(10, 3);
+        System.out.println(number);
     }
 }
