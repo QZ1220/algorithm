add073bbf38ca696adb0f84325041efddef35958
Audi <audi.car@qq.com>
2023-08-09 22:44:29 +0800

    add solution for 221

--- src/com/audi/leetcode/dp/MaximalSquare.java
+++ src/com/audi/leetcode/dp/MaximalSquare.java
@@ -35,4 +35,33 @@ public class MaximalSquare {
 
         return maxSide * maxSide;
     }
+
+
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] dp = new int[row][col];
+        int maxSide = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        // dp数组的维度比matrix要大一行、一列
+                        // dp[i][j]表示以(i, j)为右下角顶点的最大正方形的边长。
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSide * maxSide;
+    }
 }
