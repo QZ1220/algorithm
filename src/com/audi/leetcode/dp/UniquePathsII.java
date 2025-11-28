d9dca7ea234802e0cb1a91bb681de8e47f2199f4
QZ1220 <audi.car@qq.com>
2025-11-28 16:48:00 +0800

    refine code

--- src/com/audi/leetcode/dp/UniquePathsII.java
+++ src/com/audi/leetcode/dp/UniquePathsII.java
@@ -57,4 +57,44 @@ public class UniquePathsII {
         return dp[m - 1][n - 1];
     }
 
+
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        // dp[i][j]表示走到位置i，j可能的路径数量（注意：这里不是走到位置（i，j）需要的步数）
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < obstacleGrid.length && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i <obstacleGrid[0].length && obstacleGrid[0][i] == 0 ; i++) {
+            dp[0][i] = 1;
+        }
+
+        // 递推公式：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            for (int j = 1; j < obstacleGrid[0].length; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        // 打印dp数组
+        for (int i = 0; i < obstacleGrid.length; i++) {
+            for (int j = 0; j < obstacleGrid[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+    }
+
+    public static void main(String[] args) {
+        UniquePathsII uniquePathsII = new UniquePathsII();
+        System.out.println(uniquePathsII.uniquePathsWithObstacles2(new int[][]{
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        }));
+    }
+
 }
