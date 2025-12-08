6b80c5eed6dfab6706a2b7ded36d8f1b56c0ff7e
QZ1220 <audi.car@qq.com>
2025-12-08 17:25:46 +0800

    refine code

--- src/com/audi/leetcode/stack/LargestRectangleinHistogram.java
+++ src/com/audi/leetcode/stack/LargestRectangleinHistogram.java
@@ -1,18 +1,18 @@
-package com.audi.leetcode.slide.window;
+package com.audi.leetcode.stack;
 
 /**
  * https://leetcode.com/problems/largest-rectangle-in-histogram/description/
  * <p>
- * 给定一个整型数组，每个元素代表当前位置的柱状高度，求这些柱状矩形可以构成的最大矩形面积
+ * 给定一个整型数组，每个元素代表当前位置的柱状高度，求这些柱状矩形可以构成的最大矩形面积  使用单调栈的思想求解
  * <p>
  * Input: heights = [2,1,5,6,2,3]
  * Output: 10
  * Explanation: The above is a histogram where width of each bar is 1.
  * The largest rectangle is shown in the red area, which has an area = 10 units.
- *
+ * <p>
  * Input: heights = [2,4]
  * Output: 4
- *
+ * <p>
  * 1 <= heights.length <= 105
  * 0 <= heights[i] <= 104
  *
@@ -21,9 +21,9 @@ package com.audi.leetcode.slide.window;
  */
 public class LargestRectangleinHistogram {
 
-//    public int largestRectangleArea(int[] heights) {
-//
-//    }
+    public int largestRectangleArea(int[] heights) {
+        return -1;
+    }
 
     public static void main(String[] args) {
 
